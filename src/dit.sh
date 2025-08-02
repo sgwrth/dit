3e5e7e34471c86b1a2cd4bdc9ab0d7b33d00c520
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-08-02 19:29:08 +0200

    * add: parameters '-v' and '-h' for getting script version and help * add: install.sh script * change: renamed script to dit (do in terminal)

--- src/dit.sh
+++ src/dit.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
-# Check if necessary binaries are installed on the system
+# Show version.
+if [[ "$1" == "-v" || "$1" = "" ]]; then
+    printf "dit version 0.1 [02-Aug-2025]\n"
+    printf "Run 'dit -h' for help.\n"
+    exit 0
+fi
+
+# Show help.
+if [[ "$1" == "-h" ]]; then
+    printf "Show version/help:\n"
+    printf "dit -v:\tCheck version.\n"
+    printf "dit -h:\tShow help.\n"
+    printf "\nGeneral usage:\n"
+    printf "dit [lang] [action] [dir] [search term] [replacement term]\n"
+    printf "\n[lang]:\n"
+    printf "%s\t%s\n" "-c" "C"
+    printf "%s\t%s\n" "-cpp" "C++"
+    printf "\n[action]:\n"
+    printf "%s\t%s\n" "-f" "Find text in files."
+    printf "%s\t%s\n" "-m" "Make source (and header) file."
+    printf "%s\t%s\n" "-r" "Search for and replace text."
+    printf "\n[dir]:\n"
+    printf "%s\t%s\n" "-d" "Sets dir to './src/'."
+    printf "Otherwise, give the (relative) path to the dir you want to use.\n"
+    printf "The path can end in '/' (default behaviour when using tab to\n"
+    printf "autocomplete) or not, both are fine.\n"
+    printf "\n[search term], [replacement term]:\n"
+    printf "Give the string you want to find and its replacement.  When a term\n"
+    printf "consists of multiple words, enclose it in quotes.\n"
+    printf "\nExamples:\n"
+    printf "dit -c -f -d 42\n"
+    printf "\tSearches all *.c and *.h files in ./src/ and its subdirs\n"
+    printf "\tfor the term '42' and lists the occurances.\n"
+    printf "dit -cpp -m ./src/structs Person\n"
+    printf "\tCreates 'Person.cpp' and 'Person.h' files in ./src/structs dir.\n"
+    printf "\tThe files include basic C/C++ boilerplate code ('#ifndef ...').\n"
+    printf "dit -cpp -r ./src/classes Foo Bar\n"
+    printf "\tSearches all *.cpp and *.h files in ./src/classes and its subdirs\n"
+    printf "\tfor the term 'Foo' and replaces them with 'Bar'.\n"
+    printf "\n"
+    exit 0
+fi
+
+# Check if necessary binaries are installed on the system.
 required_bins=(
     "awk"
     "find"
@@ -19,13 +62,7 @@ for required_bin in "${required_bins[@]}"; do
         printf "Error: required program '${required_bin}' missing\n"
     fi
 done
-if [[ bin_missing ]]; then
-    exit 1
-fi
-
-# Exit with error if less than four params (minimum for '-f' action) were passed.
-if [[ "$#" -lt 4 ]]; then
-    printf "Error: missing parameter(s)\n"
+if [[ "$bin_missing" == true ]]; then
     exit 1
 fi
 
@@ -37,10 +74,16 @@ case "$lang" in
     "-cpp")
         ;;
     *)
-        printf "Error: invalid language argument \"${1}\".\n"
+        printf "Error: invalid parameter '${1}'.\n"
         exit 1
         ;;
 esac
+
+# Exit with error if less than four params (minimum for '-f' action) were passed.
+if [[ "$#" -lt 4 ]]; then
+    printf "Error: missing parameter(s)\n"
+    exit 1
+fi
 
 # Determine action.
 mode=$2
@@ -52,7 +95,7 @@ case $mode in
     "-r") # Replace.
         ;;
     *)
-        printf "Error: invalid action argument \"${2}\".\n"
+        printf "Error: invalid action argument '${2}'.\n"
         exit 1
         ;;
 esac
